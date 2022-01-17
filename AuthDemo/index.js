529e0f0c7273842e80984660369a42cb124e2f9b
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-17 12:59:51 +0100

    registering

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://localhost:27017/loginDemo',
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTON ERROR!!!")
+        console.log(err)
+    })
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true })); //parse req body
+
+app.get('/', (req, res) => {
+    res.send('HOME PAGE')
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+//POST ROUTE
+app.post('/register', async (req, res) => {
+    // res.send(req.body); //  vratice {"username":"Darko","password":"123456"}
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12); //$2b$12$YEjsi0bUhdYUENRmummVn.K/XJB9dADwZDhAL6.LRTIRHuoY/F2NC
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/')
 })
 
 app.get('/secret', (req, res) => {
